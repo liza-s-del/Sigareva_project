c249a6a01e7a69525426e54b1456d34564ac22f9
Liza <lizasigareva21@gmail.com>
2026-03-10 13:01:28 +0300

    сделано задание 1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,22 @@
-jkjb = 0
+student_name = 'Сигарева Елизавета Андреевна'
+group_number = '3140801/52502'
+project_name = 'ТЦ "ЕВРОПОЛИС"'
+floors = 4
+height = 12
+is_residential = False
+construction_year = 2009
+
+print("ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА")
+print(f"Студент: {student_name}")
+print(f"Группа: {group_number}")
+print(f"Объект: {project_name}")
+print(f"Этажность: {floors} этажей")
+print(f"Высота: {height} м")
+print(f"Тип: {'Жилой' if is_residential else 'Нежилой'}")
+print (f"Год постройки: {construction_year} год")
+
+"""
+Объект расположен по адресу: Полюстровский проспект, 84.
+
+Выбран этот объект, потому что это первый торговый центр, который приел в голову.
+"""
