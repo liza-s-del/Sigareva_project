0dc9fe0a63059d7519019c4c99ca18d65868a1fb
Liza <lizasigareva21@gmail.com>
2026-04-06 20:34:13 +0300

    Определение названия дня недели посредством условного оператора

--- task_04_foundation_load.py
+++ task_04_foundation_load.py
@@ -7,3 +7,21 @@
 номер дня 1-7 соответствует дням недели пн-вс
 """
 day_number = 5 #вводится число от 1 до 7
+
+#Определение названия дня недели
+if day_number = 1:
+    day_name = "Понедельник"
+elif day_number = 2:
+    day_name = "Вторник"
+elif day_number = 3:
+    day_name = "Среда"
+elif day_number = 4:
+    day_name = "Четверг"
+elif day_number = 5:
+    day_name = "Пятница"
+elif day_number = 6:
+    day_name = "Суббота"
+elif day_number = 7:
+    day_name = "Воскресенье"
+else:
+    day_name = "Ошибка: неверный номер дня. Введите число 1-7."
