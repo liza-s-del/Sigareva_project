7cfbedcdf1ee5f429d5bedc424f29d4ade35ffa6
Liza <lizasigareva21@gmail.com>
2026-04-06 20:47:05 +0300

    Вывод значений и завершение задания

--- task_04_foundation_load.py
+++ task_04_foundation_load.py
@@ -9,19 +9,19 @@
 day_number = 5 #вводится число от 1 до 7
 
 #Определение названия дня недели
-if day_number = 1:
+if day_number == 1:
     day_name = "Понедельник"
-elif day_number = 2:
+elif day_number == 2:
     day_name = "Вторник"
-elif day_number = 3:
+elif day_number == 3:
     day_name = "Среда"
-elif day_number = 4:
+elif day_number == 4:
     day_name = "Четверг"
-elif day_number = 5:
+elif day_number == 5:
     day_name = "Пятница"
-elif day_number = 6:
+elif day_number == 6:
     day_name = "Суббота"
-elif day_number = 7:
+elif day_number == 7:
     day_name = "Воскресенье"
 else:
     day_name = "Ошибка: неверный номер дня. Введите число 1-7."
@@ -45,3 +45,12 @@ elif 6 <= day_number <= 7:
 else:
     work_mode = "Ошибка"
     day_type = "Ошибка"
+
+#Вывод
+print()
+print("РАБОЧИЙ ГРАФИК")
+print()
+print(f"Номер дня: {day_number}")
+print(f"День недели: {day_name}")
+print(f"Тип дня: {day_type}")
+print(f"Режим работы: {work_mode}")
